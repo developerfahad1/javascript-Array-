0c16f111c1b4e8d5c9a1ac382ba886afdb86e7c8
developerfahad1 <fahadgraphic.pk>
2024-03-10 13:01:52 +0500

    indes.html and app.js file created

---
+++ app.js
@@ -0,0 +1,10 @@
+let cities = ['islamabad' , 'lahore' , 'peshawar' , 'sakar' , 'Attock']
+console.log(cities);
+
+let fruit = ['apple' , 'orange' , 'mango' , 'bannana' , 'grapes']
+console.log(fruit);
+// fruit.pop();
+// fruit.push('watermalon');
+// fruit.shift();
+// fruit.unshift('watermalon');
+// fruit.splice(2, 2 , 'peach' , 'watermalon')
